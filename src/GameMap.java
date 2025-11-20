8d939d0f0bae3d250e23f875021660065d003c44
Ishan Singh <26singhishan@gmail.com>
2025-11-20 08:37:57 -0800

    src folder made

---
+++ src/GameMap.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class GameMap extends JPanel {
+    final int tileSize = 50;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+    private ArrayList<point2D> path;
+
+    
+    public GameMap() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.yellow);
+        this.setDoubleBuffered(true);
+    }
+
+    public ArrayList<point2D> createPath(String routeCode, int startX, int startY) {
+        ArrayList<point2D> path = new ArrayList<point2D>();
+        // looping through routeCode
+        for (int i = 0; i < routeCode.length(); i++) {
+            // adding path depending on direction indicated in route code
+            if (routeCode.substring(i, i + 1).equals("D")) {
+                path.add(startX, startY + 100);
+                startY += 100;
+            }
+            if (routeCode.substring(i, i + 1).equals("U")) {
+                path.add(startX, startY - 100);
+                startY -= 100;
+            }
+            if (routeCode.substring(i, i + 1).equals("L")) {
+                path.add(startX - 100, startY);
+                startX -= 100;
+            }
+            if (routeCode.substring(i, i + 1).equals("R")) {
+                path.add(startX + 100, startY);
+                startX += 100;
+            }
+        }
+        return path;
+    }
+
+    
+
+}
--- src/Main.java
+++ src/Main.java
@@ -14,4 +14,5 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+    
 }
